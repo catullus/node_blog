bdae2c75988b277f2d6fb344eeb0ad03782bc381
catullus <cody.flagg@gmail.com>
2016-09-05 15:23:04 -0600

    connected to mongoDB on mongolab

--- readme.txt
+++ readme.txt
@@ -2,6 +2,8 @@ resources
 
 // main blog: http://zellwk.com/blog/crud-express-mongodb/
 
+// a similar tutorial: http://www.clock.co.uk/blog/a-simple-website-in-nodejs-with-express-jade-and-stylus
+
 https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
 
 // for nodemon setup (constantly refresh server to browser automatically)
@@ -50,6 +52,20 @@ node server.js
 
 // open page in your browser
 //browser url>> localhost:3000
+
+// how to find and kill localhost process (if it keeps running)
+// cmd >>
+netstat -a -o -n
+// find which PID is on 3000
+taskkill /pid 5440 // if PID is 5440
+
+// add body-parser library to help express handle html form data
+// cmd>> 
+npm install body-parser --save
+
+// incorporate body parser into server.js
+// txt >>
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-const express = require('express');
+const express = require('express'); // load express
+const bodyParser = require('body-parser'); // middleware bc express can't read data from <form> element
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
-// setup listener at local host port 3000
-app.listen(3000, function() {
-  console.log('listening on 3000')
-});
+// extract data from form, add them to body of the request object
+app.use(bodyParser.urlencoded({extended: true}))
+
+// set templating engine
+app.set('view engine', 'ejs')
+
+// Handlers below:
+
+mongodb://<dbuser>:<dbpassword>@ds019966.mlab.com:19966/starwars
+
+var db
+
+// setup a server that talks to a mongo database
+MongoClient.connect('mongodb://hun:<password>@ds019806.mlab.com:19806/yabba-ranks', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, function() {
+    console.log('listening on 3000')
+  })
+})
 
 /*
 // return the following package e.g. text
@@ -20,9 +38,32 @@ app.get('/', (req, res) => {
 })
 */
 
+/* // deprecated because we are pulling index.html from the index.ejs template now
+// get the HTML page index.html
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
   console.log('loading from', __dirname);
   // Note: __dirname is the path to your current working directory. Try logging it and see what you get!
   // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
 });
+*/
+
+// save data submitted from form to database
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+// create an array of quotes from the .ejs template
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
+})
